fb118b55523d526e2f6fdfa9ee04177b961a8881
MuathAlawi Alawi <muathalawi1@gmail.com>
2020-07-05 23:28:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+
+
+function theBeatlesPlay(musician, instrument) {
+ var art = [];
+ for (let i = 0; i < musician.length; i++){
+    
+     art.push(`${musician[i]} plays ${instrument[i]}`);
+     
+  }
+  return art;
+}
+
+function johnLennonFacts(facts) {
+  while() {
+    
+  }
+}
