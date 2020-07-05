82279d8a3b7ec8513d5b20d6e0c5a9ef62f24289
MuathAlawi Alawi <muathalawi1@gmail.com>
2020-07-05 23:36:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,14 +12,11 @@ function theBeatlesPlay(musician, instrument) {
 }
 
 function johnLennonFacts(facts) {
-  const facts = ["He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"];
-  
-  var 
-  while() {
-    facts.push("!!!");
+  var array = [];
+  var i = 0;
+  while(array.length < facts.length) {
+    array.push(facts[i] + "!!!");
   }
   
+  return array;
 }
